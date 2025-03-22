e08ae26dd78aeebce3c84d3819c62aa666ca4f19
Stiliyan <hristovstilian94@gmail.com>
2025-03-22 23:29:45 +0200

    Main Pages Final

---
+++ Restaurant_Manager/wwwroot/js/slider.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slider img');
+    let currentIndex = 0;
+
+    setInterval(() => {
+        currentIndex++;
+
+        if (currentIndex >= slides.length) {
+            currentIndex = 0;
+        }
+
+        const scrollAmount = slides[currentIndex].offsetLeft;
+
+        slider.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    }, 15000);
+});
